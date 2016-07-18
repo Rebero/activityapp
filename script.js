accf74d96cc94c4bb707ab211bcf0e460caca8b2
nick_d <ngdaniele@gmail.com>
2016-07-17 20:47:46 -0700

    script.js

--- script.js
+++ script.js
@@ -10,39 +10,39 @@ angular.module('mainModule', ['ngRoute'])
 
     // route for the home page
     .when('/', {
-      templateUrl : 'pages/home.html',
-      controller  : 'mainController'
+      templateUrl : 'pages/login.html',
+      controller  : 'loginController'
     })
 
     // route for home page
-    .when('/home', {
-      templateUrl : 'pages/home.html',
-      controller  : 'mainController'
+    .when('/login', {
+      templateUrl : 'pages/login.html',
+      controller  : 'loginController'
     })
 
     // route for the about page
-    .when('/about', {
-      templateUrl : 'pages/about.html',
-      controller  : 'aboutController'
+    .when('/pending', {
+      templateUrl : 'pages/pending.html',
+      controller  : 'pendingController'
     })
 
     // route for the contact page
-    .when('/contact', {
-      templateUrl : 'pages/contact.html',
-      controller  : 'contactController'
+    .when('/joined', {
+      templateUrl : 'pages/joined.html',
+      controller  : 'joinedController'
     });
   })
 
   // create the controller and inject Angular's $scope
-  .controller('mainController', function($scope) {
+  .controller('loginController', function($scope) {
     // create a message to display in our view
     $scope.message = 'Everyone come and see how good I look!';
   })
 
-  .controller('aboutController', function($scope) {
+  .controller('pendingController', function($scope) {
     $scope.message = 'Look! I am an about page.';
   })
 
-  .controller('contactController', function($scope) {
+  .controller('joinedController', function($scope) {
     $scope.message = 'Contact us! JK. This is just a demo.';
   });
