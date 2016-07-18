d82cf8db4b0f0a061301630d6c8b3be13d7df7bb
Rebero <eber.rene@gmail.com>
2016-07-18 11:34:24 -0700

    Hook up creat component to database

--- app/create.js
+++ app/create.js
@@ -1,6 +1,6 @@
-angular.module('createModule', [dataServiceModule])
+angular.module('createModule', [])
   // create the controller and inject Angular's $scope
-  .controller('createController', function($scope) {
+  .controller('createController', function($scope, Activities) {
 
     $scope.onSubmit = function(newActivity) {
       Activities.createActivity(newActivity);
