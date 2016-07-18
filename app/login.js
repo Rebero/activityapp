36b287e5f1cc373ce13d49ea573102adacc0661a
Joshua Fong <jwfong89@gmail.com>
2016-07-18 14:41:37 -0700

    rebase

--- app/login.js
+++ app/login.js
@@ -1,5 +1,9 @@
 
   angular.module('loginModule', [])
   // create the controller and inject Angular's $scope
-  .controller('loginController', function($scope) {
+  .controller('loginController', function($scope, Storage) {
+    $scope.onSubmit = function(username) {
+      Storage.login(username);
+      console.log(username);
+    }
   })
