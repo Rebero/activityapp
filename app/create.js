3008ca7cde8a1ab1ae68b4ff588014f2e242b4d6
Rebero <eber.rene@gmail.com>
2016-07-18 11:22:46 -0700

    dsd

--- app/create.js
+++ app/create.js
@@ -1,8 +1,9 @@
-angular.module('createModule', [])
+angular.module('createModule', [dataServiceModule])
   // create the controller and inject Angular's $scope
   .controller('createController', function($scope) {
 
     $scope.onSubmit = function(newActivity) {
+      Activities.createActivity(newActivity);
       $scope.user = {};
     };
   });
